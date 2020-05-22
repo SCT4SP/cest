d5e08d0ff44d083afda7a0ae91cbf14279830375
Paul Keir <paul.keir@uws.ac.uk>
2020-05-22 13:22:03 +0100

    Added tests also for const_iterator.

--- tests/iterator_tests.hpp
+++ tests/iterator_tests.hpp
@@ -23,37 +23,40 @@ constexpr auto iterator_test1() {
   return it1==it2;
 }
 
-template <typename T1, typename T2>
-constexpr auto iterator_test2() {
-  using it1_t = typename T1::iterator;
-  using it2_t = typename T2::iterator;
-  using dt1_t = typename std::iterator_traits<it1_t>::difference_type;
-  using dt2_t = typename std::iterator_traits<it2_t>::difference_type;
-  using vt1_t = typename std::iterator_traits<it1_t>::value_type;
-  using vt2_t = typename std::iterator_traits<it2_t>::value_type;
-  using p1_t  = typename std::iterator_traits<it1_t>::pointer;
-  using p2_t  = typename std::iterator_traits<it2_t>::pointer;
-  using r1_t  = typename std::iterator_traits<it1_t>::reference;
-  using r2_t  = typename std::iterator_traits<it2_t>::reference;
-  using ic1_t = typename std::iterator_traits<it1_t>::iterator_category;
-  using ic2_t = typename std::iterator_traits<it2_t>::iterator_category;
+template <typename It1, typename It2>
+constexpr void iterator_test_helper() {
+  using dt1_t = typename std::iterator_traits<It1>::difference_type;
+  using dt2_t = typename std::iterator_traits<It2>::difference_type;
+  using vt1_t = typename std::iterator_traits<It1>::value_type;
+  using vt2_t = typename std::iterator_traits<It2>::value_type;
+  using p1_t  = typename std::iterator_traits<It1>::pointer;
+  using p2_t  = typename std::iterator_traits<It2>::pointer;
+  using r1_t  = typename std::iterator_traits<It1>::reference;
+  using r2_t  = typename std::iterator_traits<It2>::reference;
+  using ic1_t = typename std::iterator_traits<It1>::iterator_category;
+  using ic2_t = typename std::iterator_traits<It2>::iterator_category;
   static_assert(std::is_same_v<dt1_t,dt2_t>);
   static_assert(std::is_same_v<vt1_t,vt2_t>);
   static_assert(std::is_same_v< p1_t, p2_t>);
   static_assert(std::is_same_v< r1_t, r2_t>);
   static_assert(std::is_same_v<ic1_t,ic2_t>);
-  return true;
+}
+
+template <typename T1, typename T2>
+constexpr void static_iterator_test() {
+  using it1_t  = typename T1::iterator;
+  using it2_t  = typename T2::iterator;
+  using cit1_t = typename T1::const_iterator;
+  using cit2_t = typename T2::const_iterator;
+  iterator_test_helper<it1_t,it2_t>();
 }
 
 void iterator_tests()
 {
 #ifndef NO_STATIC_TESTS
-  using namespace std;
   static_assert(iterator_test1<cest::vector>());
-  static_assert(iterator_test2<vector<int>,      cest::vector<int>>());
-  static_assert(iterator_test2<forward_list<int>,cest::forward_list<int>>());
-//  static_assert(iterator_test2<std::set,cest::set>());
-//  static_assert(iterator_test2<std::map<int,char>,cest::map<int,char>>());
+  static_iterator_test<std::vector<int>,      cest::vector<int>>();
+  static_iterator_test<std::forward_list<int>,cest::forward_list<int>>();
 #endif
 
   assert(iterator_test1<std::vector>());

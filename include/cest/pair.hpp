11fc9c200e37faa445a57238b59409300d5cf225
Paul Keir <paul.keir@uws.ac.uk>
2020-07-14 01:04:26 +0100

    Forgot to add pair

---
+++ include/cest/pair.hpp
@@ -0,0 +1,13 @@
+#ifndef _CEST_PAIR_HPP_
+#define _CEST_PAIR_HPP_
+
+#include <pair>
+
+namespace CEST_NAMESPACE {
+
+  using std::pair;
+  using std::make_pair;
+
+} // namespace CEST_NAMESPACE
+
+#endif // _CEST_PAIR_HPP_

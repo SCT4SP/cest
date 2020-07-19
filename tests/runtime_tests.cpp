ac95bf6df446b242fde68d71f5844408db45a05b
Paul Keir <paul.keir@uws.ac.uk>
2020-07-19 18:47:28 +0100

    Enabled all tests again, now the new deque is working.

--- tests/runtime_tests.cpp
+++ tests/runtime_tests.cpp
@@ -7,12 +7,11 @@
 #include "cctype_tests.hpp"
 #include "deque_tests.hpp"
 #include "queue_tests.hpp"
-//#include "iterator_tests.hpp"
-/*#include "algorithm_tests.hpp"
+#include "iterator_tests.hpp"
+#include "algorithm_tests.hpp"
 #include "iostream_tests.hpp"
 #include "allocator_tests.hpp"
 #include "stringstream_tests.hpp"
-*/
 
 int main(int argc, char *argv[])
 {
@@ -25,11 +24,11 @@ int main(int argc, char *argv[])
   cctype_tests();
   deque_tests();
   queue_tests();
-  //iterator_tests();
-  /*algorithm_tests();
+  iterator_tests();
+  algorithm_tests();
   iostream_tests();
   allocator_tests();
   stringstream_tests();
-*/
+
   return 0;
 }

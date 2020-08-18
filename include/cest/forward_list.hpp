f88a67b2c377772b4e717150fccd1eb71b169162
Paul Keir <pkeir@outlook.com>
2020-08-18 09:09:55 +0100

    Removed unneeded GCC workaround.

--- include/cest/forward_list.hpp
+++ include/cest/forward_list.hpp
@@ -9,31 +9,6 @@
 
 namespace cest {
 
-// constexpr reinterpret_cast is prohibited in C++20 (Section 7.7 (5.15))
-// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49171
-
-// These are only needed for GCC
-
-template <typename T, typename U>
-constexpr T const_cast2(U *p) {
-  return p ? const_cast<T>(p) : nullptr;
-}
-
-template <typename T, typename U>
-constexpr T const_cast2(U &&x) {
-  return const_cast<T>(std::forward<decltype(x)>(x));
-}
-
-template <typename T, typename U>
-constexpr T static_cast2(U *p) {
-  return p ? static_cast<T>(p) : nullptr;
-}
-
-template <typename T, typename U>
-constexpr T static_cast2(U &&x) {
-  return static_cast<T>(std::forward<decltype(x)>(x));
-}
-
 template <
   class T,
   class Allocator = std::allocator<T>
@@ -186,12 +161,12 @@ struct forward_list {
   constexpr iterator erase_after(const_iterator first, const_iterator last)
   {
     node_base* p = const_cast<node_base*>(first.m_node);
-    node_base* l = const_cast2<node_base*>(last.m_node);
+    node_base* l = const_cast<node_base*>(last.m_node);
 
-    node* curr = static_cast2<node*>(p->next);
+    node* curr = static_cast<node*>(p->next);
     while (curr != l) {
       node* tmp  = curr;
-      curr = static_cast2<node*>(curr->next);
+      curr = static_cast<node*>(curr->next);
       std::destroy_at(&tmp->value);
       m_node_alloc.deallocate(tmp, 1);
     }

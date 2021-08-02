509db35db2a50951691c7a517ec456e6bc4f0612
Paul Keir <paul.keir@uws.ac.uk>
2021-08-03 00:59:40 +0100

    Added operator<=> and operator== to shared_ptr along with a new test in memory_tests.hpp (shared_ptr_compare_test), supported by a simple implementation of compare_three_way in a new compare header.

---
+++ include/cest/compare.hpp
@@ -0,0 +1,21 @@
+#ifndef _CEST_COMPARE_HPP_
+#define _CEST_COMPARE_HPP_
+
+#include <compare>
+
+namespace cest {
+
+struct compare_three_way
+{
+  template <class T, class U>
+    requires std::three_way_comparable_with<T, U>
+  constexpr auto operator()(T&& t, U&& u) const {
+    return std::forward<T>(t) <=> std::forward<U>(u);
+  }
+
+  using is_transparent = int;
+};
+
+} // namespace cest
+
+#endif //  _CEST_COMPARE_HPP_
